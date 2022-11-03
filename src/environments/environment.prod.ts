73f5823e38850cff533536f0f401e29f608f9b18
jaredtbehr <jtb0032@mix.wvu.edu>
2022-11-03 15:33:58 -0400

    Change log
    Added change log for lab 8

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,26 @@
 export const environment = {
   production: true
 };
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyD9XCEXVYsfkD3e-DAdw0ZNRuFkLIoEv_M",
+  authDomain: "human-benchmark-app.firebaseapp.com",
+  databaseURL: "https://human-benchmark-app-default-rtdb.firebaseio.com",
+  projectId: "human-benchmark-app",
+  storageBucket: "human-benchmark-app.appspot.com",
+  messagingSenderId: "38547506701",
+  appId: "1:38547506701:web:4acf3cea5c0b0d59fd3744",
+  measurementId: "G-VDM8RTTGWR"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,11 +6,25 @@ export const environment = {
   production: false
 };
 
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyD9XCEXVYsfkD3e-DAdw0ZNRuFkLIoEv_M",
+  authDomain: "human-benchmark-app.firebaseapp.com",
+  databaseURL: "https://human-benchmark-app-default-rtdb.firebaseio.com",
+  projectId: "human-benchmark-app",
+  storageBucket: "human-benchmark-app.appspot.com",
+  messagingSenderId: "38547506701",
+  appId: "1:38547506701:web:4acf3cea5c0b0d59fd3744",
+  measurementId: "G-VDM8RTTGWR"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
